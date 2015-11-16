9bd9b898f5bb1e7ac73d6f7affbb1998a9d9d794
pii5656 <microapple56@gmail.com>
2015-11-16 09:57:30 +0900

    searchengine

--- python/sec_4/searchengine.py
+++ python/sec_4/searchengine.py
@@ -39,7 +39,7 @@ class crawler:
     words = self.separatewords(text)
 
     # get URL id
-    urlid = self.getentryid('urllist', 'url', 'url')
+    urlid = self.getentryid('urllist', 'url', url)
 
     for i in range(len(words)):
       word = words[i]
@@ -88,15 +88,12 @@ class crawler:
       wordid = self.getentryid('wordlist', 'word', word)
       self.con.execute("insert into linkwords(linkid, wordid) values (%d, %d)" % (linkid, wordid))
 
-  # receive list of pages, execute depth priority search in given depth
-  # index pages
-  def crawl(self, pages, depth = 2):
-    pass
-
   # create database table
   def createindextables(self):
     pass
 
+  # receive list of pages, execute depth priority search in given depth
+  # index pages
   def crawl(self, pages, depth = 2):
     for i in range(depth):
       newpages = set()
@@ -134,3 +131,44 @@ class crawler:
     self.con.execute('create index urltoidx on link(toid)')
     self.con.execute('create index urlfromid on link(fromid)')
     self.dbcommit()
+
+class searcher:
+  def __init__(self, dbname):
+    self.con = sqlite.connect(dbname)
+
+  def __del__(self):
+    self.con.close()
+
+  def getmatchrows(self, q):
+    # string for creating query
+    fieldlist = 'w0.urlid'
+    tablelist = ''
+    clauselist = ''
+    wordids = []
+
+    # split by spaces
+    words = q.split(' ')
+    tablenumber = 0
+
+    for word in words:
+      # get word's id
+      wordrow = self.con.execute(
+          "select rowid from wordlist where word = '%s'" % word).fetchone()
+      if wordrow != None:
+        wordid = wordrow[0]
+        wordids.append(wordid)
+        if tablenumber > 0:
+          tablelist += ','
+          clauselist += ' and'
+          clauselist += 'w%d.urlid=w%d.urlid and ' % (tablenumber - 1, tablenumber)
+        fieldlist += ',w%d.location' % tablenumber
+        tablelist += 'wordlocation w%d' % tablenumber
+        clauselist += 'w%d.wordid = %d' % (tablenumber, wordid)
+        tablenumber += 1
+
+      # construct queries with separated parts
+      fullquery = 'select %s from %s where %s' % (fieldlist, tablelist, clauselist)
+      cur = self.con.execute(fullquery)
+      rows = [row for row in cur]
+
+      return rows, wordids
--- scala/sec_4/src/main/scala/SearchEngine.scala
+++ scala/sec_4/src/main/scala/SearchEngine.scala
@@ -10,12 +10,15 @@ import slick.lifted.TableQuery
 import slick.jdbc.JdbcBackend.Database;
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
+import scala.concurrent.duration._
 import scala.sys
 import scala.xml.{Node,XML}
 import scala.xml.parsing.NoBindingFactoryAdapter
+import org.sqlite.JDBC
 import org.pii5656.collective.searchengine.DB._
 
 object Crawler {
+  val ignorewords = Set("the", "of", "to", "and", "a", "in", "is", "it")
   def toNode(str: String): Node = {
     val hp = new HtmlParser
     hp.setNamePolicy(XmlViolationPolicy.ALLOW)
@@ -32,34 +35,83 @@ object Crawler {
     }
     (resulttext.toList :\ "")(_ + _)
   }
+
   def separateWords(text: String): List[String] = {
-    text.split("^[a-zA-Z]").toList
+    text.split("[^a-zA-Z]").toList.filter(x => x != "")
   }
+
   def getEntryId(table: String, field: String, value: String, createNew: Boolean = true): Int = {
-    0
-  }
-  def isIndexed(url: String): Boolean = {
-    /*
-    val u = db.run(DB.urllist.filter(_.url === url).result.head)
-    if (u != None) {
-      // val v = db.run(DB.wordlocation.filter(_.urlid === u.rowid._1).result.head)
-      u.onSuccess { case uu =>
-        val v = db.run(DB.wordlocation.filter(_.urlid === uu._1).result.head)
-        if (v != None) return true
+    def insertNewWord = {
+      val f2 = for {
+        x <- db.run(sqlu" insert into #$table (#$field) values ('#$value')")
+        last_rowid <- db.run(sql"select last_insert_rowid()".as[Int].head)
+      } yield last_rowid
+      f2.onSuccess { case last_rowid =>
+        last_rowid
       }
+      f2
     }
-    */
+    val f1 = db.run(sql"select rowid from #$table where #$field = '#$value'".as[Int].headOption)
+    f1.onSuccess { 
+      case Some(rowid) if rowid == null => None
+      case Some(rowid) => rowid 
+    }
+    Await.result(f1, Duration.Inf).getOrElse(Await.result(insertNewWord, Duration.Inf))
+  }
+
+  def isIndexed(url: String): Boolean = {
+    var bool = false
     val f = for {
      u <- db.run(DB.urllist.filter(_.url === url).result.head)
-     v <- db.run(DB.wordlocation.filter(_.urlid === u._1).result.head)
+     v <- db.run(DB.wordlocation.filter(_.urlid === u._1).result.headOption)
     } yield v
     f.onSuccess { case ff =>
-      return true
+      if (ff != None)
+        bool = true
     }
-    return false
+    bool
   }
-  def addToIndex(page: String): Unit = println("Indexing " + page)
-  def addLinkRef(page: String, url: String, linkText: String): Unit = {}
+
+  def addToIndex(url: String, feeds: Node): Int = {
+    if (this.isIndexed(url)) return 0
+  
+    println("Indexing " + url)
+    // get words
+    val text = this.getTextOnly(feeds)
+    val words = this.separateWords(text)
+
+    // get URL id
+    val urlid = this.getEntryId("urllist", "url", url)
+    println("urlid: " + urlid.toString)
+
+    for (word <- words.zipWithIndex.filter(x => !ignorewords.contains(x._1))) {
+      // wordid 存在すれば既存のID　存在しなければインサートした後のID
+      val wordid = this.getEntryId("wordlist", "word", word._1)
+      println("wordid: " + wordid.toString)
+      db.run(wordlocation.map(w => (w.urlid, w.wordid, w.location)) += (urlid, wordid, word._2))
+    }
+    return 0
+  }
+
+  def addLinkRef(urlFrom: String, urlTo: String, linkText: String): Unit = {
+    val words = this.separateWords(linkText).filter(x => !ignorewords.contains(x))
+    val fromid = this.getEntryId("urllist", "url", urlFrom)
+    val toid = this.getEntryId("urllist", "url", urlTo)
+    if (fromid == toid) return {}
+
+    val f1 = for {
+      cur <- db.run(sqlu"insert into link(fromid, toid) values (#$fromid, #$toid)")
+      linkid <- db.run(sql"select last_insert_rowid()".as[Int].head)
+    } yield linkid
+    f1.onSuccess {
+      case s => {}
+    }
+    val linkid = Await.result(f1, Duration.Inf)
+    for (word <- words) {
+      val wordid = this.getEntryId("wordlist", "word", word)
+      db.run(sqlu"insert into linkwords(linkid, wordid) values (#$linkid, #$wordid)")
+    }
+  }
 
   def crawl(pages: List[String], depth: Int): Unit = {
     if (depth == 0) sys.exit(0)
@@ -80,7 +132,7 @@ object Crawler {
     
       if(source != null && feeds != null){
         //source.close
-        addToIndex(page)
+        addToIndex(page, feeds)
         // search <a href="http://..."> in URL
         // get "http://..."
         val links = (feeds \\ "@href").map(x =>
@@ -97,8 +149,64 @@ object Crawler {
     crawl(newpages.toList, depth-1)
   }
 }
+
+object Searcher {
+  def getMatchRows(query: String): (List[Int], List[Int]) = {
+    /*
+    def buildQuery(
+      fieldlist: String, tablelist: String, clauselist: String, wordids: List[Int], tablenumber: Int
+    ): String = {
+        val words = query.split(" ")
+        for (word <- words) {
+          val wordrow = db.run(sql"select rowid from wordlist where word = #$word".as[Int].headOption)
+          wordrow.onSuccess {
+            case Some(id) if (id != null) => 
+              val wordid = wordrow.get
+              wordids += wordid
+          }
+          tablenumber match {
+            case num > 0 =>
+
+          }
+        }
+    }*/
+   var fieldlist = "w0.urlid"
+   var tablelist = ""
+   var clauselist = ""
+   var wordids: List[Int] = List()
+
+   val words = query.split(" ")
+   var tablenumber = 0
+
+   for (word <- words) {
+     val wordrow = db.run(sql"select rowid from wordlist where word = #$word".as[Int].headOption)
+     wordrow.onSuccess {
+       case Some(wordrow) if wordrow != null =>
+         val wordid = wordrow
+         wordids += wordid
+         if (tablenumber > 0) {
+           tablelist += ","
+           clauselist += " and"
+           clauselist += s"w${tablenumber-1}.urlid = w${tablenumber}.urlid and "
+         }
+         fieldlist += s",w${tablenumber}.location"
+         tablelist += s"wordlocation w${tablenumber}"
+         clauselist += s"w${tablenumber}.wordid = ${wordid.toString}"
+         tablenumber += 1
+     }
+   }
+   val cur = db.run(sqlu"select #$fieldlist from #$tablelist where #$clauselist")
+   cur.onSuccess {
+     case c =>
+       c
+   }
+   (Await.result(cur, Duration.Inf), wordids)
+  }
+}
+
 object DB {
-  val db = Database.forURL("jdbc:sqlite:searchindex", driver = "org.sqlite.JDBC")
+  //val db = Database.forURL("jdbc:sqlite:searchindex.db", driver = "org.sqlite.JDBC")
+  val db = Database.forConfig("db")
   class Link(tag: Tag) extends Table[(Int, Int, Int)](tag, "link") {
     def id = column[Int]("rowid", O.PrimaryKey)
     def fromid = column[Int]("fromid")
@@ -115,21 +223,21 @@ object DB {
   val linkwords = TableQuery[LinkWords]
 
   class WordList(tag: Tag) extends Table[(Int, String)](tag, "wordlist") {
-    def rowid = column[Int]("rowid")
+    def rowid = column[Int]("rowid", O.PrimaryKey)
     def word = column[String]("word")
     def * = (rowid, word)
   }
   val wordlist = TableQuery[WordList]
 
   class UrlList(tag: Tag) extends Table[(Int, String)](tag, "urllist") {
-    def rowid = column[Int]("rowid")
+    def rowid = column[Int]("rowid", O.PrimaryKey)
     def url = column[String]("url")
     def * = (rowid, url)
   }
   val urllist = TableQuery[UrlList]
 
   class WordLocation(tag: Tag) extends Table[(Int, Int, Int)](tag, "wordlocation") {
-    def urlid = column[Int]("urlid")
+    def urlid = column[Int]("urlid", O.PrimaryKey)
     def wordid = column[Int]("wordid")
     def location = column[Int]("location")
     def * = (urlid, wordid, location)
@@ -137,11 +245,11 @@ object DB {
   val wordlocation = TableQuery[WordLocation]
 
   def createIndexTables(db: Database): Future[Int] = {
-    db.run(sqlu"""create table urllist(url)""")
     db.run(sqlu"""create table wordlist(word)""")
+    db.run(sqlu"""create table linkwords(wordid, linkid)""")
+    db.run(sqlu"""create table urllist(url)""")
     db.run(sqlu"""create table wordlocation(urlid, wordid, location)""")
     db.run(sqlu"""create table link(fromid integer, toid integer)""")
-    db.run(sqlu"""create table linkwords(wordid, linkid)""")
     db.run(sqlu"""create index wordidx on wordlist(word)""")
     db.run(sqlu"""create index urlidx on urllist(url)""")
     db.run(sqlu"""create index wordurlidx on wordlocation(wordid)""")
